33e546734f832aac6675712f38bbf7be86bf37a6
Yoji Kakizawa <kakizawayoji@yahoo.co.jp>
2016-11-26 23:58:44 +0900

    fizzbuzz.php

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,13 +1,17 @@
 #!/usr/bin/php
 <?php
 for ($i = 1; $i <= 30; $i++) {
-    if (($i % 3) == 0 and ($i % 5) == 0)
-    echo 'FizzBuzz';
-    elseif (($i % 3) == 0)
-    echo 'Fizz';
-    elseif (($i % 5) == 0)
-    echo 'Buzz';
-    else
-    echo $i;
+    if (($i % 3) == 0 and ($i % 5) == 0){
+    	echo "FizzBuzz\n";
+    }
+    elseif (($i % 3) == 0){
+    	echo "Fizz\n";
+    }
+    elseif (($i % 5) == 0){
+    	echo "Buzz\n";
+    }
+    else{
+    	echo $i;
+    }
 }
 ?>
